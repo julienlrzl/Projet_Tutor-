622908aaaafda6c8b29c89dc336100d2979b356d
julien larzul <julien.larzul@etu.univ-lyon1.fr>
2023-10-12 17:43:22 +0200

    premier jet navbar sans commentaires finie en react

--- frontreact/src/App.js
+++ frontreact/src/App.js
@@ -1,14 +1,22 @@
-import "./App.css";
-import Navbar from "./components/Navbar.js";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css"; // Importe les styles CSS pour l'application
+import Navbar from "./components/Navbar.js"; // Importe le composant Navbar depuis le fichier Navbar.js
+import Home from "./pages/Home"; // Importe le composant Home depuis le répertoire "pages"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Importe les éléments de React Router
 
+// Définition du composant principal de l'application
 function App() {
   return (
+    /*Crée un conteneur principal avec la classe CSS "App"*/
     <div className="App">
+      {/* gère les routes de l'application */}
       <Router>
+        {/* barre de navigation présente sur toute les pages car dans le routeur
+         */}
         <Navbar />
+        {/* utilisé pour déclarer plusieurs routes et s'assure que seule la
+        première route qui correspond à l'URL actuelle est rendue. */}
         <Switch>
+          {/* définit la route pour home */}
           <Route path="/" exact component={Home} />
         </Switch>
       </Router>
--- frontreact/src/components/Navbar.js
+++ frontreact/src/components/Navbar.js
@@ -1,18 +1,35 @@
-import React from "react";
-import Logo from '../assets/pizzaLogo.png';
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder"; // Importe l'icone pour la liste de navigation (hamburger)
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="leftSide">
-        <img src={Logo}/>
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <b className="title">NETFLIX</b>
+        <div className="hiddenLinks">
+          <Link to="/">SUMMARY</Link>
+          <Link to="/Summary">ACTORS</Link>
+          <Link to="/Actors">TRAILER</Link>
+          <Link to="/Information">INFORMATION</Link>
+          <Link to="/Proposals">PROPOSALS</Link>
+        </div>
       </div>
       <div className="rightSide">
-        <Link to="/">Home</Link>
-        <Link to="/Summary">Summary</Link>
-        <Link to="/Actors">Actors</Link>
-        <Link to="/Trailer">Trailer</Link>
+        <Link to="/">SUMMARY</Link>
+        <Link to="/Summary">ACTORS</Link>
+        <Link to="/Actors">TRAILER</Link>
+        <Link to="/Information">INFORMATION</Link>
+        <Link to="/Proposals">PROPOSALS</Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
